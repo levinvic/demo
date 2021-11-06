4cfdb3dc408fbf0193193552c0156b538ce94b68
levinvic <levinvic123@gmail.com>
2021-11-06 23:40:03 +0800

    change member path

--- api/demo_app/views.py
+++ api/demo_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_user(request):
-    return render(request, 'member.html')
+    return render(request, './templates/member.html')
